54f545a0173d6f72bbeb3c52026e5239aa048bbc
MPHEMAVATHI <mphemavathi04@gmail.com>
2026-08-25 16:20:03 +0530

    Add initial Lung Cancer Prediction script

---
+++ Lung_Cancer_Prediction.py
@@ -0,0 +1,8 @@
+# Lung Cancer Prediction using CNN and Transfer Learning
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print("Lung Cancer Prediction Project")
